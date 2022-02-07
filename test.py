e22f9fc753f8818afe09c9afaf32644f55a09ac8
Matias Agelvis <Magelvisdoz@gmail.com>
2022-02-07 10:04:17 -0400

    Fix tensorflow_datasets import
    Add success message

--- test.py
+++ test.py
@@ -1,20 +1,29 @@
+success = True
 try:
     import tensorflow
 except ImportError:
     print("WARNING: TensorFlow not found")
+    success = success and false
 try:
-    import tfds
+    import tensorflow_datasets
 except ImportError:
     print("WARNING: TensorFlow-datasets not found")
+    success = success and false
 try:
     import pandas
 except ImportError:
     print("WARNING: Pandas not found")
+    success = success and false
 try:
     import numpy
 except ImportError:
     print("WARNING: Numpy not found")
+    success = success and false
 try:
     import scipy
 except ImportError:
     print("WARNING: Scipy not found")
+    success = success and false
+
+if success:
+    print("All requirements satisfied")

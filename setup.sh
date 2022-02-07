03a6195cb062d1e3f1a17501b5c5b989001b6395
Matias Agelvis <Magelvisdoz@gmail.com>
2022-02-07 10:07:19 -0400

    Change from pip to docker based install

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
+
 # check if graphics card drivers are installed
 if command -v nvidia-smi &> /dev/null
 then
-    pip install tensorflow-gpu
-else
-    pip install tensorflow
+    # pip install tensorflow-gpu
+    CONTAINER='tensorflow/tensorflow:2.7.0-gpu-jupyter'
+  else
+    # pip install tensorflow
+    CONTAINER='tensorflow/tensorflow:2.7.0-jupyter'
 fi
 
-pip install -r requirements.txt
+docker pull $CONTAINER
+# pip install -r requirements.txt
+docker run -p8888:8888 $CONTAINER
